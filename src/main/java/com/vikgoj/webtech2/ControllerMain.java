f3fef60a925849d528ac3b6a205eed714cdc65a8
Lil Hawi Vert <lilhawara@gmail.com>
2024-02-28 20:04:11 +0100

    ayy

--- src/main/java/com/vikgoj/webtech2/ControllerMain.java
+++ src/main/java/com/vikgoj/webtech2/ControllerMain.java
@@ -3,15 +3,17 @@ package com.vikgoj.webtech2;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.vikgoj.webtech2.Entities.TestUser;
 import com.vikgoj.webtech2.Entities.User;
-import com.vikgoj.webtech2.Entities.Video;
+import com.vikgoj.webtech2.Entities.Yap;
+import com.vikgoj.webtech2.Exceptions.LoginException;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -21,19 +23,8 @@ public class ControllerMain {
     private UserRepository userRepository;
 
     @Autowired
-    private VideoRepository videoRepository;
-
+    private YapRepository yapRepository;
     
-
-    @GetMapping("/register")
-    public String getRegister(Model model) {
-        if (!model.containsAttribute("user"))
-            model.addAttribute("user", new User());
-        return "register";
-    }
-
-    
-
     @GetMapping("/room")
     public String createRoom(Model model) {
         String id = UUID.randomUUID().toString();
@@ -49,17 +40,28 @@ public class ControllerMain {
         return "room"; 
     }
 
-    @GetMapping("/login")
-    public String getLogin(Model model) {
-        if (!model.containsAttribute("user"))
-            model.addAttribute("user", new User());
-        return "login";
-    }
 
     @PostMapping("/login")
-    public boolean postLogin(@RequestBody TestUser user) {
-        System.out.println("HERE! " + user.getUsername() + " " + user.getPassword());
-        return false;
+    public ResponseEntity postLogin(@RequestBody User user) throws LoginException {
+        Optional<User> userFromRepo = userRepository.findById(user.getUsername());
+        if(userFromRepo.isPresent() && userFromRepo.get().getPassword().equals(user.getPassword())) return new ResponseEntity<>(HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
     }
+
+    @PostMapping("/register")
+    public ResponseEntity postRegister(@RequestBody User user) {
+        if(!userRepository.existsByUsername(user.getUsername())) {
+            userRepository.save(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/yap")
+    public ResponseEntity postYap(@RequestBody Yap yap) {
+        this.yapRepository.save(yap);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
 
 }
---
+++ src/main/java/com/vikgoj/webtech2/Exceptions/LoginException.java
@@ -0,0 +1,7 @@
+package com.vikgoj.webtech2.Exceptions;
+
+public class LoginException extends Exception {
+    public LoginException() {
+        super();
+    }
+}
